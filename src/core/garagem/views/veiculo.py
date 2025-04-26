c0ba6d68a48a69c832afd3ecd90dfc3a636bef4c
renascm_ <renatochaags.m@gmail.com>
2025-04-26 13:02:13 -0300

    FEAT: veiculo viewset criado

---
+++ src/core/garagem/views/veiculo.py
@@ -0,0 +1,7 @@
+from rest_framwork.viewsets import ModelViewset
+from core.garagem.models import Veiculo
+from core.garagem.serializers import VeiculoSerializer 
+
+class VeiculoViewSet(ModelViewset):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
