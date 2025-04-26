d88b14bbe5f135cbb1b037defa39ec9380e9a53d
renascm_ <renatochaags.m@gmail.com>
2025-04-26 12:35:31 -0300

    FEAT: modelo serializer criado

--- src/core/garagem/serializers/__init__.py
+++ src/core/garagem/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .acessorio import AcessorioSerializer 
 from .cor import CorSerializer
+from .modelo import ModeloSerializer
---
+++ src/core/garagem/serializers/modelo.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from core.garagem.models import Modelo
+
+class ModeloSerializer(ModelSerializer):
+    class Meta:
+        model = Modelo
+        fields = '__all__'
