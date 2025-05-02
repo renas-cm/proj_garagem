9af66b4a92f9d6b69eafba56d510e669286e76e9
renascm <renatochagas.m@gmail.com>
2025-05-02 12:31:31 -0300

    FIX: admin register

--- src/core/garagem/admin.py
+++ src/core/garagem/admin.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+from .models import Acessorio, Cor, Modelo, Veiculo
 
+admin.site.register(Acessorio)
+admin.site.register(Cor)
+admin.site.register(Modelo)
+admin.site.register(Veiculo)
