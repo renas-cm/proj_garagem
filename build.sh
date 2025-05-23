811dd8cae74504d434be1f21db7b7f9760af989c
renascm_ <renatochagas.m@gmail.com>
2025-05-23 14:14:05 -0300

    REFINE: update build.sh to use PDM for dependency management and streamline script

--- build.sh
+++ build.sh
@@ -1,39 +1,14 @@
-# #!/usr/bin/env bash
-# # Sai do script se houver algum erro
-# set -o errexit
-
-# # Atualiza o pip
-# pip install --upgrade pip
-
-# # Instala as dependências
-# pip install -r requirements.txt
-
-# # Coleta os arquivos estáticos
-# python manage.py collectstatic --no-input
-
-# # Aplica as migrações
-# python manage.py migrate
-
-
 #!/usr/bin/env bash
-# Sai do script se houver algum erro
-set -e
+set -o errexit
 
-echo "Atualizando pip (opcional, mas seguro)"
-pip install --upgrade pip
+# Atualiza o PDM
+pdm self update
 
-echo "Instalando PDM no ambiente de build"
-pip install pdm
+# Instala as dependências
+pdm install
 
-echo "Instalando dependências do projeto com PDM"
-pdm sync --prod  # Use --prod para instalar apenas dependências de produção
-
-echo "Coleta os arquivos estáticos"
+# Coleta os arquivos estáticos
 pdm run python manage.py collectstatic --no-input
 
-echo "Aplica as migrações"
+# Aplica as migrações
 pdm run python manage.py migrate
-
-# Se você tiver testes que quer rodar no build, pode adicionar aqui:
-# echo "Rodando testes"
-# pdm run python manage.py test
