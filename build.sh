5cd2f1a748580db5e0abf409d6bd51bb8329bbf9
renascm_ <renatochagas.m@gmail.com>
2025-05-23 14:15:50 -0300

    REFINE: update build.sh to replace PDM with pip for dependency management

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+# Sai do script se houver algum erro
 set -o errexit
 
-# Atualiza o PDM
-pdm self update
+# Atualiza o pip
+pip install --upgrade pip
 
 # Instala as dependências
-pdm install
+pip install -r requirements.txt
 
 # Coleta os arquivos estáticos
-pdm run python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Aplica as migrações
-pdm run python manage.py migrate
+python manage.py migrate
